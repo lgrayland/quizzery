d1fdc33d8c8cb9f933484ed71bcbceeaed4bc8e3
Luke Grayland <lgrayland@gmail.com>
2015-02-06 15:40:47 +0000

    added role to Users for cancan

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206153258) do
+ActiveRecord::Schema.define(version: 20150206153951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 20150206153258) do
     t.string   "nick_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
