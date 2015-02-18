1ecea079532d92af39ead24f1a1a7101ae35f3e3
Luke Grayland <lgrayland@gmail.com>
2015-02-18 21:20:21 +0000

    added a home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,25 @@
 //= require foundation
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){ $(document).foundation();
+  var text = $('.typewriter').text();
+
+  var length = text.length;
+  var timeOut;
+  var character = 0;
+
+
+  (function typeWriter() {
+      timeOut = setTimeout(function() {
+          character++;
+          var type = text.substring(0, character);
+          $('.typewriter').text(type);
+          typeWriter();
+          
+          if (character == length) {
+              clearTimeout(timeOut);
+          }
+          
+      }, 200);
+  }());
+});
