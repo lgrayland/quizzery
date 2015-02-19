2b1d340174d0dac5718f8804c8b26fb71848271b
Luke Grayland <lgrayland@gmail.com>
2015-02-19 00:18:20 +0000

    edit to cancan abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,18 +6,7 @@ class Ability
     if user.role? :admin
       can :manage, :all
     elsif user.persisted?
-      can :create, Game
-      can :read, Game
-      can :update, Game
-      can :destroy, Game
-      can :create, Quiz
-      can :read, Quiz
-      can :update, Quiz
-      can :destroy, Quiz
-      can :create, Question
-      can :read, Question
-      can :update, Question
-      can :destroy, Question
+      can :manage, :all
     else
 
     end
