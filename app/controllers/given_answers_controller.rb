f2aef7301292f6a4d9ec061f0c88b415ca058ee1
Luke Grayland <lgrayland@gmail.com>
2015-03-07 13:40:33 +0000

    removed commented out code in given answer controller

--- app/controllers/given_answers_controller.rb
+++ app/controllers/given_answers_controller.rb
@@ -1,12 +1,10 @@
 class GivenAnswersController < ApplicationController
-
-
+  
   respond_to :html
 
   def create
     game = Game.find(params[:game_id])
-    # answer_choice = AnswerChoice.find(params[:answer_choice_id])
-    # quiz_question = QuizQuestion.find(params[:quiz_question_id])
+
     participation = game.participation_for(current_user)
 
     given_answer = participation.given_answers.new(quiz_question_id: params[:quiz_question_id], answer_choice_id: params[:answer_choice_id], game_id: params[:game_id] )
@@ -23,9 +21,4 @@ class GivenAnswersController < ApplicationController
     end
     respond_with(game)
   end
-
-  # private
-  #   def given_answer_params
-  #     params.permit(:participation_id, :quiz_question_id, :answer_choice_id)
-  #   end
 end
